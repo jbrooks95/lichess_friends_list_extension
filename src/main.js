b1d8bd4d1e2ba60ad9ff9b6718b5d9b5c9d17e9b
James Brooks <JamesBrooks1@boisestate.edu>
2020-08-20 14:35:41 -0600

    fix bug when logged out

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 var friendBox = document.getElementsByClassName('friend_box_title')[0];
+if(friendBox != null) {
+    //calling .click() immediately does not work so it is called
+    //on an interval until the friends box has been successfully opened
+    var interval = setInterval(function () {
+        //ensure lichess has created socket
+        if (window.lichess == null || window.lichess.socket == null) {
+            return;
+        }
 
-//calling .click() immediately does not work so it is called
-//on an interval until the friends box has been successfully opened
-var interval = setInterval(function () {
-    //ensure lichess has created socket
-    if (window.lichess == null || window.lichess.socket == null) {
-        return;
-    }
+        friendBox.click(); 
 
-    friendBox.click(); 
-
-    //friend box contains <i></i> by default
-    //it contains <strong></strong> after being opened
-    if (friendBox.firstChild.nodeName !== 'I') {
-        clearInterval(interval);
-    }
-}, 200);
+        //friend box contains <i></i> by default
+        //it contains <strong></strong> after being opened
+        if (friendBox.firstChild.nodeName !== 'I') {
+            clearInterval(interval);
+        }
+    }, 200);
+}
